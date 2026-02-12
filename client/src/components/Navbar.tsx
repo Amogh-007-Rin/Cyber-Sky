502959ca501440486ea51b6a893796f7a4ba2ab8
Amogh-007-Rin <amoghdath233@gmail.com>
2026-02-12 16:35:46 +0000

    Added-server-Elements

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+    return (
+        <>
+          <div className="bg-black h-6 w-auto flex justify-evenly items-center ">
+            <nav className="bg-black h-6 w-auto flex justify-evenly items-center">
+                <ul className="h-6 w-2xl flex justify-evenly text-white items-center font-serif">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>References</li>
+                    <li>Information</li>
+                </ul>
+            </nav>
+          </div>
+        </>
+    )
+}
---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+export default function LandingPage(){
+
+    return(
+        <>
+            <Navbar/>
+            <div>This is a Landing Page</div>
+
+        
+        </>
+    )
+}
+
